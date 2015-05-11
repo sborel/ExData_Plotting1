953caafa2d81eb8481c5446cf793eaabfcc51ba1
Santa Borel <santaborel@gmail.com>
2015-05-10 20:15:55 -0400

    changed axis on plot3.png

--- plot3.R
+++ plot3.R
@@ -14,7 +14,7 @@ png("plot3.png",width = 480, height = 480)
 plot(data$datetime,data$Sub_metering_1,
      type = "l",
      xlab = "",
-     ylab = "Global Active Power (kilowatts)")
+     ylab = "Energy sub metering")
 lines(data$datetime,data$Sub_metering_2, col="red")
 lines(data$datetime,data$Sub_metering_3, col="blue")
 legend('topright',c('Sub_metering_1','Sub_metering_2','Sub_metering_3'))
